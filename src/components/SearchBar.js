d6920f7cb3f1f7a7140f022970302f316cd2d8a7
iamtash <natashachernis@gmail.com>
2020-02-12 15:28:42 -0600

    set up project file structure

---
+++ src/components/SearchBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class SearchBar extends React.Component {
+    state = {}
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <input type="text" />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchBar
