39c3517c550480faaa9e93c5bdf15b33fd582184
iamtash <natashachernis@gmail.com>
2020-02-12 18:50:53 -0600

    create ImageList component, pass it images prop

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,24 @@
 import React from 'react'
-import axios from 'axios'
+import unsplash from '../api/unsplash'
 import SearchBar from './SearchBar'
+import ImageList from './ImageList'
 
 class App extends React.Component {
-    state = { list: [] }
-    
-    async onSearchSubmit(term) {
-        const response = await axios
-            .get('https://api.unsplash.com/search/photos', {
-                params: { query: term },
-                headers: {
-                    Authorization: 'Client-ID cc5d2593dd12b09a7925fb9c8ff2836609a2f333cfb2cffb5320ff9ea61614b3'
-                }
-            })
-        console.log(response.data.results)
+    state = { images: [] }
+
+    onSearchSubmit = async term => {
+        const response = await unsplash.get('search/photos', {
+            params: { query: term }
+        })
+       
+        this.setState({ images:response.data.results })
     }
 
     render() {
         return (
             <div className="ui container" style={{marginTop: '10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                <ImageList images={this.state.images} />
             </div>
         )
     }
