8d2a345b4dae5e39e5f85ce3073d488a0ad698ed
iamtash <natashachernis@gmail.com>
2020-02-12 19:28:11 -0600

    map over / render images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const ImageList = props => {
-    console.log(props.images)
+    const images = props.images.map(image => {
+        return <img key={image.id} src={image.urls.regular} alt="car" />
+    })
 
-    return <div>ImageList</div>
+    return <div>{images}</div>
 }
 
 export default ImageList
